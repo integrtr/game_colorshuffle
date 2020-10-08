78dd247058dab9d396d38ccfa6dc98d3b6055044
mvrikxix <akhil.sabu@integrtr.com>
2020-10-08 18:41:11 +0530

    refactor: changes text from clicks to spin

--- src/components/SpinCounter.jsx
+++ src/components/SpinCounter.jsx
@@ -5,7 +5,7 @@ export function SpinCounter({ counter }) {
   return (
     <div className="counter">
       <h5>
-        <FormattedMessage id="NO_OF_CLICKS" /> <span>{counter}</span>
+        <FormattedMessage id="NO_OF_SPINS" /> <span>{counter}</span>
       </h5>
     </div>
   );
